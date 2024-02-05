3a744df0ecc6dcd2202f80fbc582aab528694659
danielfriis <danielfriis@users.noreply.github.com>
2024-02-05 08:42:33 +0000

    deploy: 501144edcd623a7a15cc80b7311271055467d669

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_pages/**/*.html',
+    './_posts/*.md',
+    './*.html'
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Inter var', ...defaultTheme.fontFamily.sans],
+      'serif': ['Baskervville', ...defaultTheme.fontFamily.serif]
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
+
